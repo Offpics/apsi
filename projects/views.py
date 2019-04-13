d97cd3c6743c37328ff1e2b430e270b9fb9ee2a4
Aquakor <kornelux1@gmail.com>
2019-04-13 15:24:33 +0200

    .

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from .models import Project
 
 
-class ProjectCreateView(LoginRequiredMixin, CreateView):
+class ProjectCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Project
     fields = ['title', 'description', 'user']
+    success_message = 'Project succesfully created!' 
 
 
 class ProjectDetailView(DetailView):
