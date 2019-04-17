d3d37f0d25db18ce9207e5319827db6976a32a65
Aquakor <kornelux1@gmail.com>
2019-04-17 13:53:31 +0200

    Add user groups

--- projects/views.py
+++ projects/views.py
@@ -1,23 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from .models import Project
 
 
-class ProjectCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+class ProjectCreateView(LoginRequiredMixin,
+                        PermissionRequiredMixin,
+                        SuccessMessageMixin,
+                        CreateView):
     model = Project
     fields = ['title', 'description', 'user']
-    success_message = 'Project succesfully created!' 
+    success_message = 'Project succesfully created!'
+
+    permission_required = 'projects.add_project'
 
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(PermissionRequiredMixin, DetailView):
     model = Project
 
+    permission_required = 'projects.view_project'
 
-class ProjectListView(ListView):
+
+class ProjectListView(PermissionRequiredMixin, ListView):
     model = Project
     context_object_name = 'projects'
+
+    permission_required = 'projects.view_project'
+
 
 def home(request):
     return render(request, 'projects/home.html')
