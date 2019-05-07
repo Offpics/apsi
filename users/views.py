33b659b390796bf2f0251c8065d8c59d42bc08e3
Aquakor <kornelux1@gmail.com>
2019-05-07 17:10:25 +0200

    Add User Registration for Admin

--- users/views.py
+++ users/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.contrib.auth.decorators import permission_required
 from .forms import UserRegisterForm
 
 
+# Only admin can add new user.
+@permission_required('auth.add_user', raise_exception=True)
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in.')
-            return redirect('login')
+            messages.success(request, f'User account: {username} has been created!')
+            return redirect('register')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
