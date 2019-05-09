6eb8047a37f6b596ede879adaa7b017ae1d7b340
Aquakor <kornelux1@gmail.com>
2019-05-09 17:43:34 +0200

    Change Projects urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -3,7 +3,6 @@ from .views import (
     home,
     ProjectCreateView,
     ProjectDetailView,
-    ProjectListView,
     MyProjectsListView,
     ProjectUpdateView,
     TaskCreateView,
@@ -13,15 +12,26 @@ from .views import (
 )
 
 urlpatterns = [
+    # Home page.
     path('', home, name='project-home'),
+
+    # Project model.
     path('projects/', MyProjectsListView.as_view(), name='project-list'),
-    path('projects/new/', ProjectCreateView.as_view(), name='project-create'),
-    path('projects/<int:pk>/', ProjectDetailView.as_view(),
+    path('projects/<int:pk>/',
+         ProjectDetailView.as_view(),
          name='project-detail'),
-    path('projects/<int:pk>/update', ProjectUpdateView.as_view(), name='project-update'),
-    # path('myprojects/', MyProjectsListView.as_view(), name='myprojects'),
-    path('projects/task/new/<int:pk>/', TaskCreateView.as_view(), name='task-create'),
-    path('projects/task/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
-    path('projects/task/update/<int:pk>/', TaskUpdateView.as_view(), name='task-update'),
-    path('projects/datepoint/new/<int:pk>/', DatePointCreateView.as_view(), name='datepoint-create'),
+    path('projects/new/', ProjectCreateView.as_view(), name='project-create'),
+    path('projects/update/<int:pk>/',
+         ProjectUpdateView.as_view(),
+         name='project-update'),
+
+    # Task model.
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+    path('task/new/<int:pk>/', TaskCreateView.as_view(), name='task-create'),
+    path('task/update/<int:pk>/',
+         TaskUpdateView.as_view(),
+         name='task-update'),
+    path('datepoint/new/<int:pk>/',
+         DatePointCreateView.as_view(),
+         name='datepoint-create'),
 ]
