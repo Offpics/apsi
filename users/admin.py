80a7a7815c330db442dad1c108a6fb35c26cbc02
Aquakor <kornelux1@gmail.com>
2019-05-12 14:49:22 +0200

    Update User Registration with groups field

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from django.contrib import admin
 
-# Register your models here.
+
+UserAdmin.add_fieldsets = (
+    (None, {"fields": ("username", "password1", "password2", "groups")}),
+)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
