22a2d50669745d9902db94339d80a9e0f0ec2640
Aquakor <kornelux1@gmail.com>
2019-05-12 15:47:21 +0200

    Add customised ModelAdmins

--- projects/admin.py
+++ projects/admin.py
@@ -2,6 +2,33 @@ from django.contrib import admin
 
 from .models import DatePoint, Project, Task
 
-admin.site.register(Project)
-admin.site.register(Task)
-admin.site.register(DatePoint)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "manager", "description")
+
+    readonly_fields = ("worker",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "project")
+
+
+@admin.register(DatePoint)
+class DatePointAdmin(admin.ModelAdmin):
+    list_display = (
+        "task",
+        "worker",
+        "worked_time",
+        "date_created",
+        "description",
+    )
+
+    readonly_fields = (
+        "task",
+        "worker",
+        "worked_time",
+        "date_created",
+        "description",
+    )
