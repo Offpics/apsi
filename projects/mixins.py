fd72f7dc95120bdedbf69834fdd59f40501882eb
Aquakor <kornelux1@gmail.com>
2019-05-12 15:33:51 +0200

    Fix mixins bug

--- projects/mixins.py
+++ projects/mixins.py
@@ -9,15 +9,17 @@ class UserBelongsToProjectMixin(UserPassesTestMixin):
     def test_func(self):
         user = self.request.user
         project_pk = self.kwargs["pk"]
-        if user.groups.all()[0].name == "Worker":
-            queryset = Project.objects.filter(id=project_pk, worker=user)
-            if queryset.count() > 0:
-                return True
-        elif user.groups.all()[0].name == "Manager":
-            queryset = Project.objects.filter(id=project_pk, manager=user)
-            if queryset.count() > 0:
-                return True
-        return False
+        if user.groups.count() > 0:
+            if user.groups.all()[0].name == "Worker":
+                queryset = Project.objects.filter(id=project_pk, worker=user)
+                if queryset.count() > 0:
+                    return True
+            elif user.groups.all()[0].name == "Manager":
+                queryset = Project.objects.filter(id=project_pk, manager=user)
+                if queryset.count() > 0:
+                    return True
+        else:
+            return False
 
 
 class UserBelongsToTaskMixin(UserPassesTestMixin):
@@ -26,12 +28,18 @@ class UserBelongsToTaskMixin(UserPassesTestMixin):
     def test_func(self):
         user = self.request.user
         task_pk = self.kwargs["pk"]
-        if user.groups.all()[0].name == "Worker":
-            queryset = Task.objects.filter(id=task_pk, project__worker=user)
-            if queryset.count() > 0:
-                return True
-        elif user.groups.all()[0].name == "Manager":
-            queryset = Task.objects.filter(id=task_pk, project__worker=user)
-            if queryset.count() > 0:
-                return True
-        return False
+        if user.groups.count() > 0:
+            if user.groups.all()[0].name == "Worker":
+                queryset = Task.objects.filter(
+                    id=task_pk, project__worker=user
+                )
+                if queryset.count() > 0:
+                    return True
+            elif user.groups.all()[0].name == "Manager":
+                queryset = Task.objects.filter(
+                    id=task_pk, project__worker=user
+                )
+                if queryset.count() > 0:
+                    return True
+        else:
+            return False
